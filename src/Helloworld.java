12da77dbc7d8ed46e528a556d65b006b1d470adc
devyohan <ticonweb@gmail.com>
2018-02-13 15:17:33 +0900

    예제 추가

---
+++ src/Helloworld.java
@@ -0,0 +1,14 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+
+        System.out.println("Dev Yohan");
+
+        for(int i=0; i<10; i++) {
+            for(int j=0; j<10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
